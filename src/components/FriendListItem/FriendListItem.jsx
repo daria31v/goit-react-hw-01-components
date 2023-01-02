c88ceeaa19299911983602151edb537a831b1816
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-01-02 23:57:29 +0100

    commit 2

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,18 @@
+// Компонент повинен приймати кілька пропів:
+// avatar - посилання на аватар
+// name - ім'я друга
+// isOnline - буль, що сигналізує про стан друга: в мережі або ні.
+// Залежно від пропа isOnline, повинен змінюватися колір фону span.status.
+// Це можна зробити за допомогою різних CSS - класів або Styled Components.
+
+
+// import friends from "path/to/friends.json";
+// <FriendList friends={friends} />;
+
+// Компонент повинен створювати DOM наступної структури.
+
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,38 @@
+/* Дані для списку доступні у форматі JSON у файлі transactions.json. Це масив об'єктів, де кожен об'єкт описує одну транзакцію 
+з наступними властивостями:
+id — унікальний ідентифікатор транзакції
+type — тип транзакції
+amount - сума транзакції
+currency - тип валюти */
+/* Опис компонента <TransactionHistory>
+Необхідно створити компонент <TransactionHistory>, який приймає один проп items – масив об'єктів транзакцій з transactions.json. 
+Компонент створює розмітку таблиці. Кожна транзакція – це рядок таблиці. У прикладі наведена розмітка двох транзакцій. */
+
+
+// import transactions from "path/to/transactions.json";
+// <TransactionHistory items={transactions} />;
+
+<TransactionHistory >
+<table class="transaction-history">
+  <thead>
+    <tr>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr>
+      <td>Invoice</td>
+      <td>125</td>
+      <td>USD</td>
+    </tr>
+    <tr>
+      <td>Withdrawal</td>
+      <td>85</td>
+      <td>USD</td>
+    </tr>
+  </tbody>
+    </table>
+</TransactionHistory>
