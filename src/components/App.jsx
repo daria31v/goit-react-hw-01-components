86088cc3e30e2db3ce9c0de5569f0e0f6a3e67c7
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-01-05 00:13:11 +0100

    1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { Profile } from "./Profile/Profile";
+import user from '../Data/user.json'
+import Statistics from './Statistics/Statistics'
+import data from '../Data/data.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile user={user} />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
+    </>
+    
+      
+   
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,44 +7,48 @@
 // avatar — посилання на зображення
 // stats — об'єкт з інформацією про активності
 
+import PropTypes from 'prop-types';
+import user from 'Data/user.json';
+// import { UserFile } from './Profile.styled'
 
-// {/* <Profile>
-//     <div class="profile">
-//   <div class="description">
-//     <img
-//       src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-//       alt="User avatar"
-//       class="avatar"
-//     />
-//     <p class="name">Petra Marica</p>
-//     <p class="tag">@pmarica</p>
-//     <p class="location">Salvador, Brasil</p>
-//   </div>
+export const Profile = ({
+  user: { username, tag, location, avatar, stats },
+}) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src={avatar} alt="User avatar" width="150" />
+          <p>{username}</p>
+          <p>{tag}</p>
+          <p>{location}</p>
+        </div>
 
-//   <ul class="stats">
-//     <li>
-//       <span class="label">Followers</span>
-//       <span class="quantity">1000</span>
-//     </li>
-//     <li>
-//       <span class="label">Views</span>
-//       <span class="quantity">2000</span>
-//     </li>
-//     <li>
-//       <span class="label">Likes</span>
-//       <span class="quantity">3000</span>
-//     </li>
-//   </ul>
-// </div>
-// </Profile>
-//  */}
+        <ul>
+          <li>
+            <span>Followers</span>
+            <span>{stats.followers}</span>
+          </li>
+          <li>
+            <span>Views</span>
+            <span>{stats.views}</span>
+          </li>
+          <li>
+            <span>Likes</span>
+            <span>{stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// import user from 'path/to/user.json';
-
-// <Profile
-//   username={user.username}
-//   tag={user.tag}
-//   location={user.location}
-//   avatar={user.avatar}
-//   stats={user.stats}
-// />
+Profile.propTypes = {
+  user: PropTypes.exact({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.objectOf(PropTypes.number).isRequired,
+  }).isRequired,
+};
